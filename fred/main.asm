7856e020d3df1033f56ca039a194003cbca117b3
Tomek Rękawek <trekawek@gmail.com>
2023-06-01 08:54:10 +0200

    Added Fred sources

---
+++ fred/main.asm
@@ -0,0 +1,39 @@
+     OPT h+
+     .local
+     ICL "d1/FR.ASM"
+     .endl
+
+     .local
+     ICL "d2/CI.ASM"
+     .endl
+
+     .local
+     ICL "d1/SO.ASM"
+     .endl
+
+     .local
+     ICL "d1/TI.ASM"
+     .endl
+
+     .local
+     ICL "d1/SHOWPIC.ASM"
+     .endl
+
+     .local
+     ICL "d1/GR.ASM"
+     .endl
+
+     .local
+     ICL "d2/AMP.ASM"
+     .endl
+
+     OPT h-
+     INS "d2/TITLE.FNT"
+     INS "d2/DI.FNT"
+     INS "d2/GRA.DTA"
+     INS "d2/GRA.PLR"
+     INS "d2/GRA.STA"
+     INS "d2/FAZY.FNT"
+     INS "d2/FRED.PIC"
+     INS "d2/GRA.FNT"
+     INS "d2/FRED.AMC"

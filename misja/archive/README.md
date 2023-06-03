4171aba697a4ed0d4121913ea09889d3549ecf75
Tomek Rękawek <trekawek@gmail.com>
2023-06-03 19:58:35 +0200

    Added Misja

---
+++ misja/archive/README.md
@@ -0,0 +1,5 @@
+The mission.zip file was downloaded from [Atari XL/XE Source Archive](http://sources.pigwa.net/).
+
+MD5 sums of the original binaries were copied to the [checksum.md5](../checksum.md5).
+```
+```
---
+++ misja/main.asm
@@ -0,0 +1,47 @@
+     OPT h+
+     .local
+     ICL "d1/E.ASM"
+     .endl
+
+     .local
+     ICL "d1/H.ASM"
+     .endl
+
+     .local
+     ICL "d1/I.ASM"
+     .endl
+
+     .local
+     ICL "d1/M.ASM"
+     .endl
+
+     .local
+     ICL "d1/S.ASM"
+     .endl
+
+     .local
+     ICL "d1/SCREEN.ASM"
+     .endl
+
+     .local
+     ICL "d1/T.ASM"
+     .endl
+
+     .local
+     ICL "d1/W.ASM"
+     .endl
+
+     .local
+     ICL "d2/AMP.ASM"
+     .endl
+
+     OPT h-
+     INS "d2/MISSION.AMC"
+     INS "d2/ENEMY.FNT"
+     INS "d2/HERO.PLR"
+     INS "d2/INFO.FNT"
+     INS "d2/LEVEL1.FNT"
+     INS "d2/LEVEL1.DTA"
+     INS "d2/LEVEL1.STA"
+     INS "d2/SCREEN.FNT"
+     INS "d2/TITLE.FNT"
